1368c649dcf05846522e667a93f63805e545bb56
Ponnada Neeeraj Vardhan <neeraj.vardhan@indiamart.com>
2022-08-13 18:27:40 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StudentComponent } from './student/student.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule
